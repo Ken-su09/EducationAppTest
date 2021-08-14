a0192cdfd90897488575e8f48ee01adf6898be56
Ken Suon <ken.su_dev@hotmail.com>
2021-08-14 18:15:00 +0200

    Add verification email with regex in Login and Sign Up

--- app/src/main/java/com/suonk/educationapptest/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/suonk/educationapptest/ui/activities/LoginActivity.kt
@@ -3,8 +3,15 @@ package com.suonk.educationapptest.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.widget.addTextChangedListener
 import com.suonk.educationapptest.R
 import com.suonk.educationapptest.databinding.ActivityLoginBinding
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
@@ -22,6 +29,18 @@ class LoginActivity : AppCompatActivity() {
 
         initializeActionBar()
         initializeButtons()
+
+        binding.loginEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                checkEmail()
+            }
+        })
     }
 
     private fun initializeActionBar() {
@@ -35,5 +54,36 @@ class LoginActivity : AppCompatActivity() {
         binding.signUpButton.setOnClickListener {
             startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
         }
+    }
+
+    private fun checkEmail() {
+        val emailPattern = Pattern.compile("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+        val userEmail = binding.loginEmail.text.toString()
+
+        if (userEmail.trim().matches(emailPattern.toRegex())) {
+            binding.loginEmailValidation.visibility = View.VISIBLE
+            binding.loginEmailValidation.setImageDrawable(
+                AppCompatResources.getDrawable(
+                    this,
+                    R.drawable.ic_check_email
+                )
+            )
+        } else {
+            if (userEmail.isEmpty()) {
+                binding.loginEmailValidation.visibility = View.INVISIBLE
+            } else {
+                binding.loginEmailValidation.visibility = View.VISIBLE
+                binding.loginEmailValidation.setImageDrawable(
+                    AppCompatResources.getDrawable(
+                        this,
+                        R.drawable.ic_check_email_cross
+                    )
+                )
+            }
+        }
+//        if (userEmail.isEmpty()) {
+//            Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+//        } else {
+//        }
     }
 }
--- app/src/main/java/com/suonk/educationapptest/ui/activities/SignUpActivity.kt
+++ app/src/main/java/com/suonk/educationapptest/ui/activities/SignUpActivity.kt
@@ -3,8 +3,13 @@ package com.suonk.educationapptest.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
 import com.suonk.educationapptest.R
 import com.suonk.educationapptest.databinding.ActivitySignUpBinding
+import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -22,6 +27,18 @@ class SignUpActivity : AppCompatActivity() {
 
         initializeActionBar()
         initializeButtons()
+
+        binding.signUpEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                checkEmail()
+            }
+        })
     }
 
     private fun initializeActionBar() {
@@ -34,5 +51,36 @@ class SignUpActivity : AppCompatActivity() {
         binding.signUpLogInButton.setOnClickListener {
             startActivity(Intent(this@SignUpActivity, LoginActivity::class.java))
         }
+    }
+
+    private fun checkEmail() {
+        val emailPattern = Pattern.compile("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+        val userEmail = binding.signUpEmail.text.toString()
+
+        if (userEmail.trim().matches(emailPattern.toRegex())) {
+            binding.signUpEmailValidation.visibility = View.VISIBLE
+            binding.signUpEmailValidation.setImageDrawable(
+                AppCompatResources.getDrawable(
+                    this,
+                    R.drawable.ic_check_email
+                )
+            )
+        } else {
+            if (userEmail.isEmpty()) {
+                binding.signUpEmailValidation.visibility = View.INVISIBLE
+            } else {
+                binding.signUpEmailValidation.visibility = View.VISIBLE
+                binding.signUpEmailValidation.setImageDrawable(
+                    AppCompatResources.getDrawable(
+                        this,
+                        R.drawable.ic_check_email_cross
+                    )
+                )
+            }
+        }
+//        if (userEmail.isEmpty()) {
+//            Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+//        } else {
+//        }
     }
 }
