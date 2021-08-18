53e446d92051a39e227b77452b84e32cbc07fe70
Ken Suon <ken.su_dev@hotmail.com>
2021-08-18 16:11:43 +0200

    If User login, even if App is kill, stay connected

--- app/src/main/java/com/suonk/educationapptest/ui/activities/WelcomeActivity.kt
+++ app/src/main/java/com/suonk/educationapptest/ui/activities/WelcomeActivity.kt
@@ -3,17 +3,28 @@ package com.suonk.educationapptest.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.suonk.educationapptest.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWelcomeBinding
 
+    // Firebase
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initializeUI()
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null) {
+            startActivity(Intent(this@WelcomeActivity, MainActivity::class.java))
+        } else {
+            initializeUI()
+        }
     }
 
     private fun initializeUI() {
